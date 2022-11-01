c984accf16b743edb77907efb18c445a7b1c5e79
yana.svnk <ianasavinkova10@gmail.com>
2022-11-01 23:52:37 +0300

    проверьте д/з, пожалуйста

---
+++ src/Author.java
@@ -0,0 +1,18 @@
+public class Author {
+    private final String name;
+    private final String surname;
+
+    public Author (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String surname () {
+        return surname;
+    }
+    }
+
+
